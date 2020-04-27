8cff865aa18747fda2d776f481c47ab23f23383c
Alex Weaver <weaver.alex.d@gmail.com>
2020-04-27 00:10:32 -0500

    Add tests

--- src/color.rs
+++ src/color.rs
@@ -22,3 +22,40 @@ fn clamp(x: f64, min: f64, max: f64) -> f64 {
         x
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::vec3::Vec3;
+
+    #[test]
+    fn clamp_returns_expected() {
+        assert_eq!(2., clamp(1., 2., 3.));
+        assert_eq!(3., clamp(4., 2., 3.));
+        assert_eq!(2.5, clamp(2.5, 2.0, 3.0));
+    }
+
+    #[test]
+    fn color_conversion_handles_supersampling() {
+        let color = to_color(Vec3::new(5., 5., 5.), 5);
+        assert_eq!(255, color.x() as i32);
+        assert_eq!(255, color.y() as i32);
+        assert_eq!(255, color.z() as i32);
+    }
+
+    #[test]
+    fn color_conversion_applies_gamma_correction() {
+        let color = to_color(Vec3::new(0.5, 0.5, 0.5), 1);
+        assert_eq!(181, color.x() as i32);
+        assert_eq!(181, color.y() as i32);
+        assert_eq!(181, color.z() as i32);
+    }
+
+    #[test]
+    fn color_conversion_clamps_unreasonable_values() {
+        let color = to_color(Vec3::new(3.0, 2.0, 0.5), 1);
+        assert_eq!(255, color.x() as i32);
+        assert_eq!(255, color.y() as i32);
+        assert_eq!(181, color.z() as i32);
+    }
+}
